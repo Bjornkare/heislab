acaa2b50a902bf688fa53650df69c74bc0c840b8
Bjorn Kare <bjornkare@ntebb.no>
2019-04-02 10:44:26 +0200

    manuell merge

--- source/fsm.c
+++ source/fsm.c
@@ -5,7 +5,34 @@
 #include "timer.h"
 #include "stdio.h"
 
+void door_timer(){
+  int wait_time = 3000; //3000 ms = 3 sec
+  clock_t start_time = clock();
+  clock_t time_diff;
+  do{
+      if (elev_get_stop_signal()){
+        break;
+      }
 
+      if ()
+
+      time_diff = (clock() - start_time) * 1000/CLOCKS_PER_SEC;
+
+  } while(time_diff < wait_time);
+}
+
+void add_order(int floor, elev_button_type_t button, int * orders){
+  orders[floor][button] = 1;
+  elev_set_button_lamp(button, floor, 1);
+}
+
+void remove_order(int floor, elev_button_type_t button, int * orders){
+  &orders[floor][button] = 0;
+  orders[floor][2] = 0;
+
+  elev_set_button_lamp(button, floor, 0);
+  elev_set_button_lamp(BUTTON_CMD, floor, 0);
+}
 
 void fsm_init() {
 	fsm_data.orders = {
@@ -66,4 +93,8 @@ bool check_for_stop(int floor, int dir){
    }
   else dir_index = dir;
   return (orders[floor][0] || orders[floor][dir_index]);
+}
+
+bool check_for_stop(int floor, elev_button_type_t dir){
+  return (orders[floor][2] || orders[floor][dir]);
 }
