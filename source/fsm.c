99b9c11010739322d1f5a5e134341910a1d71390
haavafa <havardfageras@hotmail.com>
2019-04-04 10:31:09 +0200

    Update fsm.c

--- source/fsm.c
+++ source/fsm.c
@@ -7,13 +7,13 @@
 
 void door_timer(){
   elev_set_door_open_lamp(1);
+  while (elev_get_stop_signal()) {
+	  delete_all_orders()
+  };
   int wait_time = 3000; //3000 ms = 3 sec
   clock_t start_time = clock();
   clock_t time_diff;
   do{
-      if (elev_get_stop_signal()){
-        break;
-      }
 
       if ()
 
@@ -51,7 +51,7 @@ void fsm_init() {
 		{ 0,0,0 }
 	};
 	fsm_data.active_state = IDLE;
-	int curr_floor = 0;
+	int prev_floor = 0;
 	int curr_dir = 0;
 }
 
@@ -59,7 +59,7 @@ int fsm_evt_order(int floor, elev_button_type_t dir) {
 	add_order(floor, dir);
 	switch (fsm_data.active_state) {
 	case IDLE:
-		if(floor != fsm_data.curr_floor){
+		if(floor != fsm_data.prev_floor){
 			fsm_data.curr_dir = idle_get_dir()
 			elev_set_motor_direction(fsm_data.curr_dir);
 			fsm_data.active_state = MOVING;
@@ -79,7 +79,7 @@ int fsm_evt_order(int floor, elev_button_type_t dir) {
 
 
 void fsm_evt_floor_sensor(int floor) {
-	fsm_data.curr_floor = floor;
+	fsm_data.prev_floor = floor;
 	elev_set_floor_indicator(floor);
 	switch (fsm_data.active_state) {
 	case IDLE:
@@ -87,7 +87,7 @@ void fsm_evt_floor_sensor(int floor) {
 	case MOVING:
 		if (check_for_stop(floor, fsm_data.dir)) {
 			elev_set_motor_direction(DIRN_STOP);
-			active_state = state.DOOR_OPEN;
+			fsm_data.active_state = state.DOOR_OPEN;
 			door_timer();
 		}
 		break;
@@ -95,6 +95,24 @@ void fsm_evt_floor_sensor(int floor) {
 	case STOPPED:
 		break;
 }
+	
+void fsm_evt_stop_button_pressed(){
+	elev_set_motor_direction(DIRN_STOP);
+	if (elev_get_floor_sensor_signal() != -1) {
+		fsm_data.active_state = state.DOOR_OPEN;
+		door_timer();
+	}
+	else {
+		fsm_data.active_state = state.STOPPED;
+		while (elev_get_stop_signal) {
+		}
+		delete_all_orders();
+		fsm_data.active_state = state.MOVING;
+		elev_set_motor_direction(fsm_data.curr_dir);
+	}
+}
+
+
 
 bool check_for_stop(int floor, int dir){
   int dir_index;
